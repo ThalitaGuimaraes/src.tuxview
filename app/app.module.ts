75cd15bc1b73e6e686564110c94da302ff01cc75
ThalitaGuimaraes <102105342+ThalitaGuimaraes@users.noreply.github.com>
2022-10-26 20:57:45 -0300

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {FotoComponent} from './foto/foto.component';
+
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotoComponent,
+    HeaderComponent,
+    InicioComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
