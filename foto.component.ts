2cefa991039feee60d76dd436333cfc352b67d45
ThalitaGuimaraes <102105342+ThalitaGuimaraes@users.noreply.github.com>
2022-11-03 21:39:06 -0300

    Add files via upload

--- foto.component.ts
+++ foto.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 
 @Component({
@@ -7,23 +8,25 @@ import { Component } from "@angular/core";
 })
 
 export class FotoComponent{
-
+nameButton = "Fotos Pares";
 titulo = "Minhas Fotos";
+control = true;
 
-imagens = [
-    {imagem: "https://cdn.pixabay.com/photo/2022/08/31/10/20/buttercup-7422987__340.jpg",titulo:"Flor amarela"},
-    {imagem: "https://cdn.pixabay.com/photo/2022/09/01/04/40/anemone-7424709__340.jpg",titulo:"Flor verde"},
-    {imagem: "https://cdn.pixabay.com/photo/2022/08/01/13/20/lily-of-the-valley-7358144__340.jpg",titulo:"Flor branca"},
-    {imagem: "https://cdn.pixabay.com/photo/2022/10/10/06/12/yellow-flower-7510901__340.jpg",titulo:"Flor colorida"},
-    {imagem: "https://cdn.pixabay.com/photo/2022/08/11/09/53/hummingbird-hawk-moth-7379059__340.jpg",titulo:"Flor vermelha"},
-    {imagem: "https://cdn.pixabay.com/photo/2022/09/15/09/47/sunflower-7456101__340.jpg",titulo:"Girassol"},
-  ];
-  
-  clicar(){
-    if(this.titulo == "Minhas Fotos"){
-      this.titulo = "Minhas Imagens";
+imagens = [];
+
+constructor (private http: HttpClient) {
+
+}
+
+  mudar(){
+
+    this.control = !this.control;
+    
+    if(this.nameButton == "Todas Imagens" ){
+      this.nameButton = "Fotos Pares"
     }else{
-      this.titulo = "Minhas Fotos";
+      this.nameButton = "Todas Imagens";
+    }
     }
   }
-}
+  
